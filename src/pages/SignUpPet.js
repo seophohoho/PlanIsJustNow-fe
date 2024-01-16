b9ed3ab1b7d76470c239b76319fbcaef28445fec
young24n <won8560@gmail.com>
2024-01-16 15:02:16 +0900

    상단 navbar 추가

--- src/pages/SignUpPet.js
+++ src/pages/SignUpPet.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
-import { Form, Col, Row, Button, Image, Container } from 'react-bootstrap';
+import { Form, Col, Row, Button, Image, Container, Navbar } from 'react-bootstrap';
 
 function SignUpPet() {
     const [petImage, setPetIamge] = useState(['Example Path',])
     
   return (
     <div>
+        <header>
+            <Navbar expand="md" className="bg-body-tertiary">
+                <Container>
+                <Navbar.Brand href="#">
+                    <img src='/logo192.png'width={"50px"}></img>
+                </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </header>
+        <body>
         <PetCircleImage/>
-        
+
+        </body>
+        <footer>
+
+        </footer>        
     </div>
   );
 }
@@ -19,9 +33,9 @@ function PetCircleImage(props){
             <Row>
                 <Form.FloatingLabel>
                     <Col>
-                    <Image src={'/thumbnail.png'} roundedCircle className='input-bgSet w-25'/>
+                    <Image src={'/thumbnail.png'} roundedCircle className='pet-image'/>
                     </Col>
-                    <label className='center'>이름</label>
+                    <label className='pet-image'>이름</label>
                 </Form.FloatingLabel>
             </Row>
             </Container>
