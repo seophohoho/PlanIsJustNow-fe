95434c94c3eb2c6bffb630fde7e4646657115654
young24n <won8560@gmail.com>
2024-01-16 16:25:50 +0900

    타이틀 제작

--- src/pages/SignUpPet.js
+++ src/pages/SignUpPet.js
@@ -9,15 +9,29 @@ function SignUpPet() {
         <header>
             <Navbar expand="md" className="bg-body-tertiary">
                 <Container>
-                <Navbar.Brand href="#">
-                    <img src='/logo192.png'width={"50px"}></img>
-                </Navbar.Brand>
+                    <Navbar.Brand href="#">
+                        <img src='/logo192.png'width={"50px"}></img>
+                    </Navbar.Brand>
                 </Container>
             </Navbar>
         </header>
-        <body>
-        <PetCircleImage/>
 
+        <h1 className='page-title'>펫 선택하기</h1>
+
+        <body>
+            <Container>
+                <Row>
+                    <Col>
+                        {/* 좌측 펫 선택지 */}
+                        <PetCircleImage/>
+                    </Col>
+                    <Col>
+                        <PetCircleImage/>
+
+                        {/* 우측 */}
+                    </Col>
+                </Row>
+            </Container>
         </body>
         <footer>
 
@@ -30,14 +44,14 @@ function PetCircleImage(props){
     return(
         <div>
             <Container>
-            <Row>
-                <Form.FloatingLabel>
+                <Row>
                     <Col>
-                    <Image src={'/thumbnail.png'} roundedCircle className='pet-image'/>
+                        <Image src={'/thumbnail.png'} roundedCircle className='pet-image'/>
+                        <label className='pet-image'>이름</label>
                     </Col>
-                    <label className='pet-image'>이름</label>
-                </Form.FloatingLabel>
-            </Row>
+                    <Col>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
