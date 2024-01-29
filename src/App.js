945c5b486ec1adb76eea991210c00e9225d54657
young24n <won8560@gmail.com>
2024-01-29 10:26:53 +0900

    /signup 경로에 이동하도록 라우팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import Signup from './pages/SignUp';
 import SignUpPet from './pages/SignUpPet';
 import Login from './pages/Login'
 
 function App() {
   return (
+    <>
     <Login/>
+    
+    <Routes>
+      <Route path='/signup' element={Signup}/>
+    </Routes>
+    </>
   );
 }
 
