2d022dd548d548d9b161d4cb2f697fc2e863d56e
young24n <won8560@gmail.com>
2024-01-31 15:50:44 +0900

    레이아웃 추가, Navbar 추가

--- src/pages/CalenderMain.js
+++ src/pages/CalenderMain.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import { Form, Col, Row, Container, Navbar } from 'react-bootstrap';
 
 const CalendarMain = () => {
   
@@ -10,11 +11,36 @@ const CalendarMain = () => {
   ]
 
   return (
-     <FullCalendar 
-       plugins={[dayGridPlugin]} 
-       initialView="dayGridMonth" 
-       events={events} /*events 배열은 달력에 표시될 이벤트 목록이다.*/
-     />
+    <div>
+         <header>
+            <Navbar expand="md" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#">
+                        <img src='/logo192.png'width={"50px"}></img>
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </header>
+        <body>
+            <Container>
+                <Row>
+                    <Col>
+                        <FullCalendar 
+                        plugins={[dayGridPlugin]} 
+                        initialView="dayGridMonth" 
+                        events={events} /*events 배열은 달력에 표시될 이벤트 목록이다.*/
+                        />
+                    </Col>
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        </body>
+        <footer>
+
+        </footer>
+     </div>
    );
 }
 
