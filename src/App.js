768fc6ecbf6444ebf79b6c778beb6d8ef2fba9c3
young24n <won8560@gmail.com>
2024-01-31 14:21:44 +0900

    캘린더 라우팅 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Signup from './pages/SignUp';
 import SignUpPet from './pages/SignUpPet';
 import Login from './pages/Login'
+import CalendarMain from './pages/CalenderMain';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={ <Signup/> }/>
+        <Route path='/Calender' element={<CalendarMain/>}/>
         <Route path="*" element={ <div>잘못된 경로입니다!</div> } />
       </Routes>
     </>
