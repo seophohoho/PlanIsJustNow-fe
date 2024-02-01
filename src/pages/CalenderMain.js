71224aad49369415e42e17c5289cc2a88c296ec7
young24n <won8560@gmail.com>
2024-02-01 16:32:45 +0900

    달력 필요 속성 추가

--- src/pages/CalenderMain.js
+++ src/pages/CalenderMain.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import '../styles/CalenderMain.css'
 import { Form, Col, Row, Container, Navbar } from 'react-bootstrap';
 
 const CalendarMain = () => {
   
   const events = [
-    { title: 'Sample Event', date: new Date() },
+    { title: 'Sample Event', date: "2024-02-02" },//date는 클릭된 이벤트의 날자 state를 가져와서 적용 title은 일정의 일부분을 가져옴
     // more events...
   ]
 
@@ -25,10 +26,17 @@ const CalendarMain = () => {
             <Container>
                 <Row>
                     <Col>
-                        <FullCalendar 
+                        <FullCalendar
                         plugins={[dayGridPlugin]} 
                         initialView="dayGridMonth" 
                         events={events} /*events 배열은 달력에 표시될 이벤트 목록이다.*/
+                        contentHeight="auto"
+                        headerToolbar={{
+                            left:'prev',
+                            center:'title',
+                            right:'next'
+                         }}
+                         locale="en"
                         />
                     </Col>
                     <Col>
