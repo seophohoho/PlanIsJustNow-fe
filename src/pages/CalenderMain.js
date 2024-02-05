56eccffe6be712f0a8bf01ebdad3678a4f8254bf
young24n <won8560@gmail.com>
2024-02-05 15:48:41 +0900

    레이아웃이 동적으로 변하도록 수정

--- src/pages/CalenderMain.js
+++ src/pages/CalenderMain.js
@@ -24,8 +24,8 @@ const CalendarMain = () => {
         </header>
         <body>
             <Container>
-                <Row>
-                    <Col sm={7}>
+                <Row className="justify-content-md-center"  >
+                    <Col xs lg="7">
                         <FullCalendar /*오늘 색-> 연보라, 클릭된날 색->? 클릭이벤트 좀따 살펴보기*/
                         plugins={[dayGridPlugin]} 
                         initialView="dayGridMonth" 
@@ -39,15 +39,15 @@ const CalendarMain = () => {
                          locale="en"
                         />
                     </Col>
-                    <Col sm={5}>
+                    <Col md="auto">
                         <Stack>{/**나중에 줄바꿈 되는 모든 div에 클래스 적용  white-space:nowrap;*/}
                             <Stack direction='horizontal' className='m-top-20' gap={3}>
-                                <Image src="/logo192.png" roundedCircle className='input-bgSet float-display image-w '/> {/**여기도 컴포넌트화 */}
+                                <Image src="/logo192.png" roundedCircle className='input-bgSet float-display image-w'/> {/**여기도 컴포넌트화 */}
                                 <div>
                                     <p>홍길동</p>{/*폰트, 줄바꿈, 공간 나중에, 받아온거 리덕스로 적용*/}
                                     <p>#123456</p>
                                 </div>
-                                <Stack direction='horizontal' className='fc-direction-ltr m-auto m-right-zero'>{/*스크롤 지원, 버튼x, 튀어나간 요소 배경에 비치치않게, state로 친구 수에 따라 늘어나도록 */}
+                                <Stack direction='horizontal' className='fc-direction-ltr m-auto'>{/*스크롤 지원, 버튼x, 튀어나간 요소 배경에 비치치않게, state로 친구 수에 따라 늘어나도록 */}
                                     <p>친구목록</p>
                                     <Stack className='text-center'>{/* todo 여기는 컴포넌트화 해서 map으로 */}
                                         <Image src="/logo192.png" roundedCircle className='input-bgSet float-display w-25 m-auto '/>
@@ -58,12 +58,14 @@ const CalendarMain = () => {
                             <Stack direction='horizontal' className='fc-direction-ltr-2v'>
                                 {/**고정된 크기, 스크롤 지원 일정 생성버튼은 일정들 맨 아래에 내부일정 또한 컴포넌트화 해서 map으로 */}
                                 <div className='h-225'>
-                                    
+
                                 </div>
                             </Stack>
                             <Stack direction='horizontal' className='fc-direction-ltr'>
                                 {/**펫 공간 위 아래, 고정된 크기 위쪽만 좀 생성   */}
-                                <div className='h-200'></div>
+                                <div className='h-200'>
+                                    
+                                </div>
                             </Stack>
                         </Stack>
                     </Col>
