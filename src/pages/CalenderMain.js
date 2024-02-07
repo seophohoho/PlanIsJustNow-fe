b907f801429064d91d781a5f883741267e2ceca7
young24n <won8560@gmail.com>
2024-02-07 15:02:05 +0900

    스케줄 컴포넌트 디자인 수정

--- src/pages/CalenderMain.js
+++ src/pages/CalenderMain.js
@@ -11,7 +11,7 @@ const CalendarMain = () => {
     { title: '물 1ml 마시기', start: "2024-02-11", end: "2024-02-14"},//end는 +1일 추가하여 적용해야함, 추측이지만 12시 기준이라 그런듯 공식 Docs에도 주의하라고만 써져있음
     { title: '스쾃999회', date: "2024-02-03", end: "2024-02-04"},
   ]
-
+  {/**서버에 있는 모든 이벤트를 한번에 모아서 합친 후 events로 변환 */}
   return (
     <div>
          <header>
@@ -48,12 +48,12 @@ const CalendarMain = () => {
                                     <p>홍길동</p>{/*폰트, 줄바꿈, 공간 나중에, 받아온거 리덕스로 적용*/}
                                     <p>#123456</p>
                                 </div>
-                                <div direction='horizontal' className='fc-direction-ltr m-auto section'>{/*스크롤 지원, 버튼x, 튀어나간 요소 배경에 비치치않게, state로 친구 수에 따라 늘어나도록 */}
+                                <div direction='horizontal' className='fc-direction-ltr m-auto section'>{/*map으로 친구 수에 따라 늘어나도록 */}
                                     <p className='section__item'>친구목록</p>
                                     <Stack className='section__item w-50'>{/* todo 여기는 컴포넌트화 해서 map으로 */}
                                         <Image src="/logo192.png" roundedCircle className='input-bgSet w-50'/>
                                         <Form.Label className=''>이름</Form.Label>
-                                    </Stack>{/**네브바쪽으로 옮길까? 충돌 무시도 있을것 같은데 살펴보자 이미지 너무커! */}
+                                    </Stack>
                                     <Stack className='section__item w-50'>{/* todo 여기는 컴포넌트화 해서 map으로 */}
                                         <Image src="/logo192.png" roundedCircle className='input-bgSet w-50  '/>
                                         <Form.Label className=''>이름</Form.Label>
@@ -75,7 +75,8 @@ const CalendarMain = () => {
                                 {/**고정된 크기, 스크롤 지원 일정 생성버튼은 일정들 맨 아래에 내부일정 또한 컴포넌트화 해서 map으로 */}
                                 <div className='h-225 w-max'>
                                     <Stack>
-                                        <Schedule/>      
+                                        <Schedule/>
+                                        <Schedule/>  
                                     </Stack>
                                 </div>
                             </Stack>
@@ -100,12 +101,13 @@ const CalendarMain = () => {
 function Schedule(){
     return(
         <Row>{/**여기서 부터 컴포넌트화 */}
-            <Col sm={1}><input type='checkbox'></input></Col>
-            <Col sm={8}>sample</Col>
-            <Col sm={1}>
-            <input type='checkbox'></input>
+            <Col sm={1}></Col>
+            <Col sm={1} className='m-auto p-zero'><input type='checkbox'></input></Col>
+            <Col sm={7} className='m-auto'>sample</Col>
+            <Col sm={1} className='m-auto'>
+                <input type="checkbox" id="cb1"/>
             </Col>
-            <Col sm={2}>
+            <Col sm={2} className='m-auto'>
                 <Button>...</Button> {/**디자인 초기화 아니면 다른요소 제작 후 클릭이벤트 구현 */}
             </Col>
         </Row>
