761acf1e31e442d5a7b409d460c58e541f1ea639
young24n <won8560@gmail.com>
2024-03-04 10:44:11 +0900

    calendar 라우팅 오타 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Signup from './pages/SignUp';
 import SignUpPet from './pages/SignUpPet';
 import Login from './pages/Login'
-import CalendarMain from './pages/CalenderMain';
+import CalendarMain from './pages/CalendarMain';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={ <Signup/> }/>
-        <Route path='/Calender' element={<CalendarMain/>}/>
+        <Route path='/Calendar' element={<CalendarMain/>}/>
         <Route path="*" element={ <div>잘못된 경로입니다!</div> } />
       </Routes>
     </>
