335f24bea6b6deb4a5d490a4e47de0039de0ebad
young24n <won8560@gmail.com>
2024-03-05 12:38:22 +0900

    일정 데이터 바인딩

--- src/components/ScheduleComponent.js
+++ src/components/ScheduleComponent.js
@@ -1,17 +1,21 @@
 import {Col, Row, Dropdown, DropdownButton} from "react-bootstrap"
+import { useSelector } from "react-redux"
 
-function Schedule(){
+function Schedule(props){
+    const state = useSelector((state)=> state)
+    const {i, clickedDate} = props
     return(
         <Row className='section__item-schedule'>
-            <Col sm={2} className='text-center'><input type='checkbox'/></Col>
-            <Col sm={7} className='m-auto'>sample</Col>
+            <Col sm={1} className='text-center'><input type='checkbox'/></Col>
+            <Col sm={1} className='m-auto'>{ state.dateSchedule[clickedDate][i].time }</Col>
+            <Col sm={6} className='m-auto'>{ state.dateSchedule[clickedDate][i].title }</Col>
             <Col sm={1} className='m-auto'>
                 <input type="checkbox" id="cb1"/>
             </Col>
             <Col sm={2} className='m-auto'>
-                <DropdownButton drop="start" variant="secondary">{/**디자인 초기화 아니면 다른요소 제작 후 클릭이벤트 구현 */}
+                <DropdownButton drop="start" variant="secondary" title="">{/**디자인 초기화 아니면 다른요소 제작 후 클릭이벤트 구현 */}
                     <Dropdown.Item eventKey="1">일정 수정</Dropdown.Item>
-                    <Dropdown.Divider />
+                    <Dropdown.Divider/>
                     <Dropdown.Item eventKey="2">삭제</Dropdown.Item>
                 </DropdownButton>             
             </Col>
