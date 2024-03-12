d9a3180521de904b2eea75e190e9fa39c64ec515
young24n <won8560@gmail.com>
2024-03-12 19:55:25 +0900

    불필요한 요소 제거

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,27 +1,14 @@
 import {Modal, Form, Button} from "react-bootstrap"
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { TimePicker, DatePicker, Space } from 'antd';
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+
+
 import "@djthoms/pretty-checkbox"
-import { Checkbox } from "pretty-checkbox-react";
 
 
 function ScheduleAddModal(props){
     const state = useSelector(state => state)
+    const dispatch = useDispatch(state=> state)
     const {confirmShow, confirmHandleClose, i, clickedDate} = props;
-    
-    const { RangePicker } = DatePicker;
-
-    dayjs.extend(customParseFormat);
-    const onChange = (time, timeString) => {
-        console.log(time, timeString);
-    };
-
-    const confirmHandler = function(e){}
-
-    const checkBoxHandler = function(e){console.log(e.target.checked)}
-    
 
     return (
       <> {/*todo 올바른 form control 할당 버튼 디자인 변경*/}
@@ -39,7 +26,7 @@ function ScheduleAddModal(props){
             <Button variant="secondary" onClick={confirmHandleClose}>
               취소
             </Button>
-            <Button variant="primary" onClick={confirmHandleClose/* dispatch(edit or add)*/}>
+            <Button variant="primary" onClick={dispatch(scheduleComplete(true))/* dispatch(complete 변경, payload로 true 전달)*/}>
               확인
             </Button>
           </Modal.Footer>
