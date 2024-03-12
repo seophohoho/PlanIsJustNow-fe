dc9d00cb239876411c23fcd2c26ef00b316aa50c
seophohoho <seop0937@gmail.com>
2024-03-12 21:03:06 +0900

    Fix git conflict

--- src/pages/CalenderMain.js
+++ src/pages/CalenderMain.js
@@ -1,24 +1,30 @@
+src/pages/CalenderMain.js
 import React from 'react';
+import { useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
+import momentPlugin from '@fullcalendar/moment';
+import interactionPlugin from '@fullcalendar/interaction'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import '../styles/CalenderMain.css'
-import PetSpaceComponent from '../components/PetSpaceComponent';
+import { formatDate } from '@fullcalendar/core'
+import moment from 'moment';
+import 'moment/locale/ko'
+import '../styles/CalendarMain.css'
+import Schedule from '../components/ScheduleComponent';
+import { scheduleInit } from '../store/store';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Col, Row, Container, Navbar, Stack, Image, Button, Dropdown, DropdownButton, SplitButton } from 'react-bootstrap';
-import { useDispatch, useSelector } from "react-redux"
-import { test } from "../store/store"//수정할 함수 import 해야함
 
 const CalendarMain = () => {
-  const state = useSelector(state =>{
-    return state;
-  })
-  const dispatch = useDispatch();
+    const state = useSelector((state)=> {return state});
+    const [clickedDate, setClickedDate] = useState("");
+    const dispatch = useDispatch();
+  /*달력 뷰에 보여지는 것은 addEvent를 이용해서 객체.important 여부 판단 후 삽입 완료된 일정은 impotant가 자동으로 false가 되어야함  */
+    const events = [
+        { title: '물 999L 마시기', date: "2024-03-02", end: "2024-03-08" },/* date는 클릭된 이벤트의 날자 state를 가져와서 적용 title은 일정의 일부분을 가져옴*/
+        { title: '물 1ml 마시기', start: "2024-03-11", end: "2024-03-14"},/* end는 +1일 추가하여 적용해야함, 추측이지만 12시 기준이라 그런듯 공식 Docs에도 주의하라고만 써져있음*/
+        { title: '스쾃999회', date: "2024-03-03", end: "2024-03-04"},
+    ]
 
-  const events = [
-    { title: '물 999L 마시기', date: "2024-02-02", end: "2024-02-08" },//date는 클릭된 이벤트의 날자 state를 가져와서 적용 title은 일정의 일부분을 가져옴
-    { title: '물 1ml 마시기', start: "2024-02-11", end: "2024-02-14"},//end는 +1일 추가하여 적용해야함, 추측이지만 12시 기준이라 그런듯 공식 Docs에도 주의하라고만 써져있음
-    { title: '스쾃999회', date: "2024-02-03", end: "2024-02-04"},
-  ]
-  {/**서버에 있는 모든 이벤트를 한번에 모아서 합친 후 events로 변환 */}
   return (
     <div>
          <header>
@@ -34,30 +40,50 @@ const CalendarMain = () => {
             <Container>
                 <Row className="justify-content-md-center"  >
                     <Col lg="7">
-                        <FullCalendar /*오늘 색-> 연보라, 클릭된날 색->? 클릭이벤트는 추후 살펴보기*/
-                        plugins={[dayGridPlugin]} 
+                        <FullCalendar /* 오늘 색-> 찐보라, 클릭된날 색-> 연보라 클릭이벤트는 추후 살펴보기 */
+                        plugins={[interactionPlugin, dayGridPlugin, momentPlugin]} 
                         initialView="dayGridMonth" 
-                        events={events} /*events 배열은 달력에 표시될 이벤트 목록이다.*/
+                        selectable={true}
+                        selectAllow={function (e) {/* 클릭 가능한 날짜를 하루로 고정 */
+                            if (e.end.getTime() / 1000 - e.start.getTime() / 1000 <= 86400) {
+                                return true;
+                            }
+                        }}
+                        dateClick={function(data) {/*클릭된 날짜 반환*/
+                            alert('Clicked on: ' + data.dateStr);
+                            setClickedDate(data.dateStr)
+                        }}
+                        datesSet={function(args) {
+                            /* 달력 초기화 시 작동 TODO: axios 일정관련 함수 또한 여기서 실행  */
+                            dispatch(scheduleInit(/*axios*/));
+                            
+                            /* 리액트에서 최상위 객체 오브젝트에 접근하려면 이렇게 해야함 */
+                            const view = args.view.calendar.currentData.currentDate;
+                            /*getMonth는 JavaScript에서 날짜의 월은 0(1월)부터 11(12월)까지 번호가 지정됨 +1을 해야 원본 값이 나옴*/
+                            /*처음 axios에서 받은 값을 초기화 후 해당값에서 아래 값으로 접근해서 map으로 나열*/
+                            const currentDate = moment().format('YYYY-MM-DD');
+                            setClickedDate(currentDate);
+                        }}
+                        events={events} /* events 배열은 달력에 표시될 이벤트 목록 */
                         contentHeight="auto"
-                        contentWidth="500"
                         headerToolbar={{
                             left:'prev',
                             center:'title',
                             right:'next'
                          }}
-                         locale="en"//지역설정, 시간관련 메소드 사용할시 해당지역으로 설정됨 주의!
+                         locale="en"/* 지역설정, 시간관련 메소드 사용할시 해당지역으로 설정됨 주의! */
                         />
                     </Col>
                     <Col lg="5">
-                        <Stack className='h-600'>{/**나중에 줄바꿈 되는 모든 div에 클래스 적용  white-space:nowrap;*/}
+                        <Stack>{/**나중에 줄바꿈 되는 모든 div에 클래스 적용  white-space:nowrap; <-- 스케줄 컴포넌트에 적용해보기 */}
                             <Stack direction='horizontal' className='m-top-20' gap={3}>
                                 <Image src="/logo192.png" roundedCircle className='input-bgSet float-display image-w'/> {/**여기도 컴포넌트화 */}
                                 <div>
-                                    <p>홍길동</p>{/*폰트, 줄바꿈, 공간 나중에, 받아온거 리덕스로 적용*/}
-                                    <p>#123456</p>
+                                    <p className='color-darkBlue'>홍길동</p>{/*폰트, 줄바꿈, 공간 나중에, 받아온거 리덕스 데이터바인딩*/}
+                                    <p className='color-violet'>#123456</p>
                                 </div>
-                                <div direction='horizontal' className='fc-direction-ltr m-auto section max-w-250'>{/*map으로 친구 수에 따라 늘어나도록 */}
-                                    <p className='section__item sticky'>친구목록</p>
+                                <div direction='horizontal' className='fc-direction-ltr m-auto section max-h-100'>{/*map으로 친구 수에 따라 늘어나도록 */}
+                                    <p className='section__item sticky color-darkBlue'>친구목록</p>
                                     <Stack className='section__item'>{/* todo 여기는 컴포넌트화 해서 map으로 */}
                                         <Image src="/logo192.png" roundedCircle className='input-bgSet w-50'/>
                                         <Form.Label className=''>이름</Form.Label>
@@ -78,48 +104,35 @@ const CalendarMain = () => {
                             </Stack>
                             <Stack direction='horizontal' className='fc-direction-ltr-2v section-schedule'>
                                 {/**고정된 크기, 스크롤 지원 일정 생성버튼은 일정들 맨 아래에 내부일정 또한 컴포넌트화 해서 map으로 */}
-                                <div className='h-max w-max'>
+                                <div className='h-225 w-max'>
+                                    {/*여기에 title 부착 sticky로, 된다면 일정 추가 버튼도 여기에?*/}
                                     <Stack>
-                                        <Schedule/>
-                                        <Schedule/>
-                                        <Schedule/>
-                                        <Schedule/>
-                                        <Schedule/>
-                                        <Schedule/>
+                                        {/*비동기 문제 &&로 해결*/
+                                            state.dateSchedule[clickedDate] && state.dateSchedule[clickedDate].map(function(notUse, i){
+                                                return(
+                                                    <Schedule i={i} clickedDate={clickedDate}/>
+                                                )
+                                            })
+                                        }
                                     </Stack>
                                 </div>
                             </Stack>
-                            <Stack direction='horizontal h-30p'>
-                                <PetSpaceComponent/>
+                            <Stack direction='horizontal' className='fc-direction-ltr'>
+                                {/* 브런치 분리됨! */}
+                                <div className='h-200'>
+                                    
+                                </div>
                             </Stack>
                         </Stack>
                     </Col>
                 </Row>
             </Container>
         </body>
-        <footer>{/**sticky footer bar or sticky header bar */}
+        <footer>{/*sticky Navbar로 결정 */}
 
         </footer>
      </div>
    );
 }
 
-function Schedule(){
-    return(
-        <Row className='section__item-schedule'>{/*추후 구조화된 폴더로 이동,리덕스 연결 */}
-            <Col sm={2} className='text-center'><input type='checkbox'/></Col>
-            <Col sm={7} className='m-auto'>sample</Col>
-            <Col sm={1} className='m-auto'>
-                <input type="checkbox" id="cb1"/>
-            </Col>
-            <Col sm={2} className='m-auto'>
-                <DropdownButton drop="start" variant="secondary">{/**디자인 초기화 아니면 다른요소 제작 후 클릭이벤트 구현 */}
-                    <Dropdown.Item eventKey="1">일정 수정</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item eventKey="2">삭제</Dropdown.Item>
-                </DropdownButton>             
-            </Col>
-        </Row>
-    )
-}
 export default CalendarMain
--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,4 @@
+src/store/store.js
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 const petImages = createSlice({//펫 이미지경로
@@ -21,19 +22,6 @@ const petName = createSlice({//펫 이름
   reducers:{
 
   }
-})
-
-const inGamePetId = createSlice({
-  name : 'inGamePetId',
-  initialState : 0,
-  reducers:{
-    //함수 정의하면 됨.
-    test(state,action){
-      state = action.payload;
-      console.log(state);
-    }
-  }
-
 })
 
 export const {} = petName.actions
@@ -61,17 +49,67 @@ const petSelected = createSlice({
   }
 })
 
+//전체 일정 보여주는 스케줄 추후에 완전히 비워야함
+const dateSchedule = createSlice({
+  name : "dateSchedule",
+  initialState : {
+    "2024-03-13" : [
+      {title : "운동", end:"2024-03-15", time: "20:00", important: true, complete : false},
+      {title : "식사", end:"2024-03-16", time: "17:00", important: false , complete : true},
+    ],
+    "2024-03-12" : [
+      {title : "회의", end:"2024-03-17", time: "12:00", important: true, complete : false},
+      {title : "간식", end:"2024-03-18", time: "15:51", important: false, complete : true},
+      {title : "후식", end:"2024-03-19", time: "18:11", important: true, complete : true},
+      {title : "가나다라마바사", end:"2024-03-20", time: "17:21", important: false, complete : true},
+      {title : "공부", end:"2024-03-21", time: "17:23", important: false, complete : false},
+
+    ]
+  },
+  reducers:{
+    scheduleInit(state, action){//state 초기화
+      return state
+    },
+    scheduleEdit(state, action){//add?
+      state.title = action.payload//payload 객체로
+      state.end = action.payload//
+      state.time = action.payload//
+      state.important = action.payload//
+    },
+    scheduleComplete(state, action){
+      state.complete = action.payload//
+      state.important = false// 완료된 일정은 자동으로 제외
+    }
+  }
+})
+
+//calendar view에 나오는 스케줄
+const events = createSlice({
+  name : "events",
+  initialState : {
+      "2024-02-25": [
+        // 해당 날짜의 다른 일정들
+      ],
+      "2024-02-05": [
+        // 해당 날짜의 다른 일정들
+      ],
+      // 추가적인 날짜와 일정들
+  },
+  reducers:{}
+})
+
+export const {scheduleInit, scheduleComplete} = dateSchedule.actions
 export const {selectPetId, selectPetName} = petSelected.actions
-export const {test} = inGamePetId.actions
 //함수또한 내보내야 요청가능
 
-export default configureStore({// 내보낼 state 작성 문법은 아래와 같음
+export default configureStore({// 내보낼 state, 작성 문법은 아래와 같음
   reducer: { 
     petImages : petImages.reducer,
     petName : petName.reducer,
     petId : petId.reducer,
     petInpo : petInpo.reducer,
     petSelected : petSelected.reducer,
-    inGamePetId: inGamePetId.reducer,
+    events : events.reducer,
+    dateSchedule :dateSchedule.reducer,
   }
 }) 
