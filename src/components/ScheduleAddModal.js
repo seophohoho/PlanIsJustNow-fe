dce6186626ac23fce3c4411b2d99051a824f8d72
young24n <won8560@gmail.com>
2024-03-14 12:37:59 +0900

    한칸 씩 당김

--- src/components/ScheduleAddModal.js
+++ src/components/ScheduleAddModal.js
@@ -2,114 +2,110 @@ import {Modal, Form, Button} from "react-bootstrap"
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { TimePicker, DatePicker, Space } from 'antd';
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
+import { scheduleStateEdit } from "../store/store";
 import "@djthoms/pretty-checkbox"
 import { Checkbox } from "pretty-checkbox-react";
 
 
 function ScheduleAddModal(props){
-    const state = useSelector(state => state)
-    const {show, handleClose, i, clickedDate} = props;
-    
-    const { RangePicker } = DatePicker;
+  const state = useSelector(state => state)
+  const dispatch = useDispatch();
+  const {show, handleClose, i, clickedDate} = props;
+  const { RangePicker } = DatePicker;
 
-    dayjs.extend(customParseFormat);
-    const onChange = (time, timeString) => {
-        console.log(time, timeString);
-    };
+  dayjs.extend(customParseFormat);
+  const timePickerHandler = (time, timeString) => {
+      console.log(time, timeString);
+  };
 
-    const confirmHandler = function(e){}
+  const confirmHandler = function(e){
+    handleClose();
+    /*조건 검사*/
+  };
 
-    const checkBoxHandler = function(e){console.log(e.target.checked)}
-    
-    const title = "일정수정";//임시, 재활용 할려면 비슷한 형식으로 해야할 듯
-    //title state에 따라 바뀌게?? 초기값, 확인버튼의 동작 이벤트를 다르게 해야함 등등
+  const checkBoxHandler = function(e){/* e.target.checked을 임시 state에 전달 */}
+  
+  const title = "일정수정";//임시, 재활용 하려면 비슷한 형식으로 해야할 듯
+  //title state에 따라 바뀌게?? 초기값, 확인버튼의 동작 이벤트를 다르게 해야함 등등
 
-    //수정의 경우 기존 데이터가 입력되어있는 형태여야 함-!-
-    //추가의 경우 완전 비어있는 상태
-    //이벤트 조건 함수 해당일의 important가 3개 초과이면 안됨
-    //모달의 확인 버튼을 누를때 해당 조건을 모두 판단하고 state를 변경하는 식으로
-    //일정의 각 항목의 정보를 나타내는 title 필요 sticky로
-    //complete의 경우 모달로 확인 사실을 확정하고 disable 하는 방식으로
+  //추가의 경우 입력 form이 완전 비어있는 상태
+  //events 추가 조건 함수 해당일의 important가 3개 초과이면 안됨--
+  //modal의 확인 버튼을 누를때 해당 조건을 모두 판단하고 state를 변경하는 식으로
+  //일정의 각 항목의 정보를 나타내는 title 필요 sticky로 일정 추가 버튼도 여기 배치
+  //complete의 경우 모달로 확인 사실을 확정하고 disable 하는 방식으로
 
+  return (
+    <> {/*todo 올바른 form control 할당 버튼 디자인 변경*/}
+      <Modal show={show} onHide={handleClose} className="p-400" >
 
-    /*
-    확인 누를때, 체크박스 -> 컨펌 모달 확인 후 확정 형식으로(disable) , 변경 내용은 
-    임시로 state에 저장후 전달 payload에 index, clickedDate, title...등 객체형으로 확인버튼만 
-    add의 경우 edit의 경우
-    체크박스는 complete만 보내기 어차피 1회
-    */
+        <Modal.Header closeButton>
+          <Modal.Title className="color-darkBlue">{ title }</Modal.Title>
+        </Modal.Header>
 
-    return (
-      <> {/*todo 올바른 form control 할당 버튼 디자인 변경*/}
-        <Modal show={show} onHide={handleClose} className="p-400" >
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-4" controlId="ControlInput1">
+              <Form.Label className="color-darkBlue">
+                  일정
+              </Form.Label>
+              <Form.Control 
+                  className="schedule-title m-left-13p"
+                  defaultValue={ state.dateSchedule[clickedDate][i].title }/>{/*글자 50자 제한 필요*/}
+            </Form.Group>
 
-          <Modal.Header closeButton>
-            <Modal.Title className="color-darkBlue">{ title }</Modal.Title>
-          </Modal.Header>
+            <Form.Group className="mb-4">
+              <Form.Label htmlFor="ControlInput2" className="color-darkBlue">날짜</Form.Label>
+              <RangePicker 
+                  className="m-left-38p"
+                  id="ControlInput2"
+                  inputReadOnly={true} 
+                  onChange={timePickerHandler}//datePickerHandler추가 후 변경 
+                  defaultOpenValue={dayjs('YYYY-MM-DD')}
+                  defaultValue={[dayjs(clickedDate, 'YYYY-MM-DD'), dayjs(state.dateSchedule[clickedDate][i].end, 'YYYY-MM-DD')]}
+                  />
+            </Form.Group>
 
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-4" controlId="ControlInput1">
-                <Form.Label className="color-darkBlue">
-                    일정
-                </Form.Label>
-                <Form.Control 
-                    className="schedule-title m-left-13p"
-                    defaultValue={ state.dateSchedule[clickedDate][i].title }/>{/*글자 50자 제한 필요*/}
-              </Form.Group>
+            <Form.Group className="mb-5">
+              <label htmlFor="ControlInput3" className="color-darkBlue">시간 선택</label>
+              <TimePicker 
+                  className="m-left-60p" 
+                  id="ControlInput3" 
+                  inputReadOnly={true} 
+                  onChange={timePickerHandler} 
+                  defaultOpenValue={dayjs('HH:mm')}
+                  defaultValue={dayjs(state.dateSchedule[clickedDate][i].time,'HH:mm')}
+                  format="HH:mm"
+                  />
+            </Form.Group>
 
-              <Form.Group className="mb-4">
-                <Form.Label htmlFor="ControlInput2" className="color-darkBlue">날짜</Form.Label>
-                <RangePicker 
-                    className="m-left-38p"
-                    id="ControlInput2"
-                    inputReadOnly={true} 
-                    onChange={onChange} 
-                    defaultOpenValue={dayjs('YYYY-MM-DD')}
-                    defaultValue={[dayjs(clickedDate, 'YYYY-MM-DD'), dayjs(state.dateSchedule[clickedDate][i].end, 'YYYY-MM-DD')]}
-                    />
-              </Form.Group>
+            <Form.Group className="mb-4" controlId="ControlInput4">
+              <Form.Label className="color-darkBlue">중요여부</Form.Label>
+              <Checkbox 
+                  animation="tada" 
+                  className="m-left-80p" 
+                  icon={<i className="zmdi zmdi-star mdc-text-amber"/>}
+                  shape="curve"
+                  onChange={checkBoxHandler}
+                  defaultChecked={state.dateSchedule[clickedDate][i].important} 
+                  />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
 
-              <Form.Group className="mb-5">
-                <label htmlFor="ControlInput3" className="color-darkBlue">시간 선택</label>
-                <TimePicker 
-                    className="m-left-60p" 
-                    id="ControlInput3" 
-                    inputReadOnly={true} 
-                    onChange={onChange} 
-                    defaultOpenValue={dayjs('HH:mm')}
-                    defaultValue={dayjs(state.dateSchedule[clickedDate][i].time,'HH:mm')}
-                    format="HH:mm"
-                    />
-              </Form.Group>
-
-              <Form.Group className="mb-4" controlId="ControlInput4">
-                <Form.Label className="color-darkBlue">중요여부</Form.Label>
-                <Checkbox 
-                    animation="tada" 
-                    className="m-left-80p" 
-                    icon={<i className="zmdi zmdi-star mdc-text-amber"/>}
-                    shape="curve"
-                    onChange={checkBoxHandler}
-                    defaultChecked={state.dateSchedule[clickedDate][i].important} 
-                    />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              취소
-            </Button>
-            <Button variant="primary" onClick={handleClose/* dispatch(edit or add)*/}>
-              확인
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            취소
+          </Button>
+          <Button variant="primary" 
+          onClick={confirmHandler}>
+            확인
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
   
 
 export default ScheduleAddModal
