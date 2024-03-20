9c989bdd069715198841ea0bda179fcdd67c7f6c
young24n <won8560@gmail.com>
2024-03-20 17:22:26 +0900

    add modal에 맞게 handler 수정, 현재 시각을 가지는 currentTime 추가, addModal 실행시 초기화 지정

--- src/components/ScheduleAddModal.js
+++ src/components/ScheduleAddModal.js
@@ -1,30 +1,29 @@
+import { useState } from "react";
 import {Modal, Form, Button} from "react-bootstrap"
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { TimePicker, DatePicker, Space } from 'antd';
+import { TimePicker, DatePicker } from 'antd';
 import { useSelector,useDispatch } from "react-redux"
-import { scheduleStateEdit } from "../store/store";
+import { addHandleClose, scheduleComplete, scheduleStateAdd } from "../store/store";
 import "@djthoms/pretty-checkbox"
 import { Checkbox } from "pretty-checkbox-react";
-
+import moment from 'moment';
 
 function ScheduleAddModal(props){
   const state = useSelector(state => state)
   const dispatch = useDispatch();
-  const {show, handleClose, i, clickedDate} = props;
-  const { RangePicker } = DatePicker;
-  const ScheduleState = state.dateSchedule[clickedDate][i];
+  const { clickedDate } = props;
+  const currentTime = moment().format('HH:MM');
+
   
+  /*초기화 상태*/
   const tempSchedule = {
     clickedDate: clickedDate,
-    index: i, 
-    title : ScheduleState.title, 
-    end: ScheduleState.end, 
-    time: ScheduleState.time, 
-    important: ScheduleState.important, 
-    complete : ScheduleState.complete
+    title : "",
+    end: clickedDate, 
+    time: currentTime,
+    important: false,
   }
-
   dayjs.extend(customParseFormat);
 
   function DatePickerHandler(notuse, picks){
@@ -38,32 +37,31 @@ function ScheduleAddModal(props){
   };
   function titleHandler(e){
     tempSchedule.title = e.target.value;
+
   }
 
-  const confirmHandler = function(e){
-    /*+조건 검사*/
-    tempSchedule.clickedDate = clickedDate;
-    tempSchedule.index = i;
+  function modalClose(){dispatch(addHandleClose())}
 
-    dispatch(scheduleStateEdit(tempSchedule))
+  const addConfirmHandler = function(e){
+    tempSchedule.clickedDate = clickedDate;
+
+    dispatch(scheduleStateAdd(tempSchedule))
+
     console.log(tempSchedule)
-    handleClose();
+    modalClose()
   };
 
   
   
-  const title = "일정수정";//임시, 재활용 하려면 비슷한 형식으로 해야할 듯
-  //title state에 따라 바뀌게?? 초기값, 확인버튼의 동작 이벤트를 다르게 해야함 등등
+  const title = "일정추가";
 
   //추가의 경우 입력 form이 완전 비어있는 상태
   //events 추가 조건 함수 해당일의 important가 3개 초과이면 안됨--
   //modal의 확인 버튼을 누를때 해당 조건을 모두 판단하고 state를 변경하는 식으로
-  //일정의 각 항목의 정보를 나타내는 title 필요 sticky로 일정 추가 버튼도 여기 배치
-  //complete의 경우 모달로 확인 사실을 확정하고 disable 하는 방식으로
 
   return (
     <> {/*todo 올바른 form control 할당 버튼 디자인 변경*/}
-      <Modal show={show} onHide={handleClose} className="p-400" >
+      <Modal show={state.addShow.show} onHide={modalClose} className="p-400" >
 
         <Modal.Header closeButton>
           <Modal.Title className="color-darkBlue">{ title }</Modal.Title>
@@ -76,10 +74,12 @@ function ScheduleAddModal(props){
                   일정
               </Form.Label>
               <Form.Control 
+                  type='input'
                   className="schedule-title m-left-13p"
-                  defaultValue={ state.dateSchedule[clickedDate][i].title }
+                  defaultValue=""
                   onChange={titleHandler}
-              />{/*글자 50자 제한 필요*/}
+                  maxLength={50}/*글자 50자 제한*/
+              />
             </Form.Group>
 
             <Form.Group className="mb-4">
@@ -90,7 +90,7 @@ function ScheduleAddModal(props){
                   inputReadOnly={true} 
                   onChange={DatePickerHandler}//datePickerHandler추가 후 변경 
                   defaultOpenValue={dayjs('YYYY-MM-DD')}
-                  defaultValue={[dayjs(state.dateSchedule[clickedDate][i].end, 'YYYY-MM-DD')]}
+                  defaultValue={[dayjs(clickedDate, 'YYYY-MM-DD')]}
                   />
             </Form.Group>
 
@@ -102,7 +102,7 @@ function ScheduleAddModal(props){
                   inputReadOnly={true} 
                   onChange={TimePickerHandler} 
                   defaultOpenValue={dayjs('HH:mm')}
-                  defaultValue={dayjs(state.dateSchedule[clickedDate][i].time,'HH:mm')}
+                  defaultValue={dayjs(dayjs(),'HH:mm')}
                   format="HH:mm"
                   />
             </Form.Group>
@@ -115,18 +115,18 @@ function ScheduleAddModal(props){
                   icon={<i className="zmdi zmdi-star mdc-text-amber"/>}
                   shape="curve"
                   onChange={checkBoxHandler}
-                  defaultChecked={state.dateSchedule[clickedDate][i].important} 
+                  defaultChecked={false} 
                   />
             </Form.Group>
           </Form>
         </Modal.Body>
 
         <Modal.Footer>
-        <Button variant="primary" 
-          onClick={confirmHandler}>
+        <Button variant="primary" className=""
+          onClick={addConfirmHandler}>
             확인
           </Button>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={modalClose}>
             취소
           </Button>
         </Modal.Footer>
