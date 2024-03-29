6926f5799ca94012357874b1a9cfd39e0cef1583
young24n <won8560@gmail.com>
2024-03-29 18:51:48 +0900

    Navbar : 유저 프로필 클래스 생성

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Form, Col, Row, Container, Navbar, Stack, Image, Button } from 'react-bootstrap';
+import { Container, Navbar, Stack, Image} from 'react-bootstrap';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Space } from 'antd';
 
 const NavbarComponent = () => {
 
@@ -12,10 +14,12 @@ const NavbarComponent = () => {
                     <img src='/logo192.png'width={"50px"}></img>
                 </Navbar.Brand>
                 {/* image는 ant-design으로 */}
-                <div>
+                <Stack className='user_profill'>
+                    <Avatar className='' size={64} icon={<UserOutlined />} />
                     <p className='color-darkBlue'>홍길동</p>{/*폰트, 줄바꿈, 공간 나중에, 받아온거 리덕스 데이터바인딩*/}
-                    <p className='color-violet'>#123456</p>
-                </div>
+                    <p className='color-violet'>#Email@example.com</p>
+                </Stack>
+                    
             </Container>
         </Navbar>
     )
