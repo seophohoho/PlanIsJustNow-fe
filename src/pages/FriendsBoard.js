876315f4312fe785f37f1469ec8cf0efb4736587
young24n <won8560@gmail.com>
2024-04-04 13:53:38 +0900

    FriendBoard : 친구 목록 데이터바인딩

--- src/pages/FriendsBoard.js
+++ src/pages/FriendsBoard.js
@@ -7,6 +7,8 @@ import { scheduleDelete } from "../store/store";
 import NavbarComponent from "../components/NavbarComponent";
 
 function TabChildrenComponent(props){ 
+  const state = useSelector((state)=>state)
+  const {i, index, user} = props
   return(
     <Row className='section__item-schedule'>
           <Col sm={3} className='text-center impo-margin-zero p-zero'>
@@ -15,23 +17,23 @@ function TabChildrenComponent(props){
             <Avatar className='' size={64} icon={<UserOutlined/>} />
           </Col>
           <Col sm={2} className='m-auto color-darkBlue p-zero'>
-            <p className='color-darkBlue text-left'>홍길동</p>{/*이름, 아이디, 모두 state에서 바인딩해야 함 로그아웃에 이벤트 넣어야함 로그인 화면으로 라우팅 하는 것도*/}
-            <p className='color-violet text-left'>#Email@example.com</p>
+          <p className='color-darkBlue text-left'>{user.userName}</p>
+        <p className='color-violet text-left'>{user.userEmail}</p></Col>
+          <Col sm={3}>
+            <Button className=''>{i === 0 ? "살펴보기" : "친구수락"}</Button>
+            <Button className='margin-left'>{i === 0 ? "친구삭제" : "거절하기"}</Button>
           </Col>
           <Col sm={3}>
-            <Button className=''>살펴보기</Button>
-            <Button className='margin-left'>친구삭제</Button>
           </Col>
-          <Col sm={3}>
-          </Col>
-      </Row>
+    </Row>
   );
 }
 
 function FriendBoard() {
     // props 로 i만 전달 tab children는 컴포로 분리후 map으로 Tabs children prop으로 전달 하는 식으로
     // redux state는 친구 수락이랑 목록으로 하고 key는 2개로
-    
+    const state = useSelector((state)=>state)
+
     return (
       <>
       <header>
@@ -48,8 +50,14 @@ function FriendBoard() {
             key: id,
             label: tabTitle[i],
             children: 
-            <Stack>
-              <TabChildrenComponent i={i}/>
+            <Stack gap={3}>{/*redux state와 i에 따라 map*/}
+              {
+                  state.friendList[i === 0 ? "userList" : "userRequest"].map((user, index) => {
+                    return (
+                      <TabChildrenComponent key={index} i={i} index={index} user={user}/>
+                    );
+                  })
+              }
             </Stack>,
             icon: <Icon />,
           };
