3049bab22135970bee2fe9e6cffa317930a8aa2d
young24n <won8560@gmail.com>
2024-04-04 14:12:23 +0900

    NavBar : Home -> MyPage 변경 로고 클릭시 calendar로 링크

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -10,13 +10,13 @@ const NavbarComponent = () => {
     return(
         <Navbar expand="md" className="bg-body-tertiary">{/**추후 Navbar도 컴포넌트화 해서 다른페이지에 적용시키기 */}
             <Container>
-                <Navbar.Brand href="#">
+                <Navbar.Brand href="/calendar">
                     <img src='/logo192.png'width={"50px"} className='m-auto'></img>
                     <h1 style={{display: "inline"}} className=''>PETTODO</h1>
                 </Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">{/*추후 아이콘 추가*/}
-                    <Nav.Link href="/calendar">Home</Nav.Link>
+                    <Nav.Link href="/user-page">MyPage</Nav.Link>
                     <Nav.Link href="/Friend-board">Frends</Nav.Link>
                     <Nav.Link href="/calendar">Petdex</Nav.Link>
                 </Nav>
