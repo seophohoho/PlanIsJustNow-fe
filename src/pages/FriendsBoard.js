b98b7b98c9e3cfbf91bba2069c18bd17d5520aed
young24n <won8560@gmail.com>
2024-04-04 11:28:53 +0900

    FriendBoard: tab 최초 시작 지점 변경, TabChildren Component 함수 생성

--- src/pages/FriendsBoard.js
+++ src/pages/FriendsBoard.js
@@ -1,13 +1,24 @@
-import {Col, Row} from "react-bootstrap"
+import { useState } from "react";
+import {Col, Row, Stack} from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 import { EllipsisOutlined, StarTwoTone } from '@ant-design/icons';
 import { Tabs } from 'antd';
 import { TeamOutlined, UserAddOutlined } from '@ant-design/icons';
-import { useState } from "react";
 import { scheduleDelete } from "../store/store";
 import NavbarComponent from "../components/NavbarComponent";
 
+function TabChildrenComponent(props){ 
+  return(
+    <div>
+      {props.i === 0 ? <span>hello1</span> : <span>hello2</span>}
+    </div>
+  );
+}
+
 function FriendBoard() {
+    // props 로 i만 전달 tab children는 컴포로 분리후 map으로 Tabs children prop으로 전달 하는 식으로
+    // redux state는 친구 수락이랑 목록으로 하고 key는 2개로
+    
     return (
       <>
       <header>
@@ -16,14 +27,17 @@ function FriendBoard() {
       <body>
       <Tabs
         className="m-auto text-center"
-        defaultActiveKey="2"
+        defaultActiveKey="0"/*tab 최초 시작지점*/
         items={[TeamOutlined, UserAddOutlined].map((Icon, i) => {
           const id = String(i + 1);
           const tabTitle = ["친구목록", "친구추가"]
           return {
             key: id,
             label: tabTitle[i],
-            children: `Hello Tab ${id}!`,
+            children: 
+            <Stack>
+              <TabChildrenComponent i={i}/>
+            </Stack>,
             icon: <Icon />,
           };
         })}
@@ -35,5 +49,6 @@ function FriendBoard() {
       </>
       );
 }
-{/*취업지원제도*/}
+
+
 export default FriendBoard;
