621cbc0ed260a2545dd6c472cb62c8ae904b42b9
young24n <won8560@gmail.com>
2024-04-05 12:46:05 +0900

    FriendBoard : state 친구 삭제 핸들러 구현

--- src/pages/FriendsBoard.js
+++ src/pages/FriendsBoard.js
@@ -3,13 +3,25 @@ import {Col, Row, Stack, Button, Form} from "react-bootstrap"
 import { useSelector, useDispatch } from "react-redux"
 import { Tabs, Avatar } from 'antd';
 import { TeamOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
-import { scheduleDelete } from "../store/store";
+import { friendDelete, friendRefuse, friendAccept } from "../store/store";
 import NavbarComponent from "../components/NavbarComponent";
 
 function TabChildrenComponent(props){ 
-  const state = useSelector((state)=>state)
-  const {i, index, user} = props
+  const state = useSelector((state)=>state.friendList)
+  const {i, index, user, list} = props
   const dispatch = useDispatch();
+
+  function deleteHandler(list, index){
+    console.log(list, index)
+    dispatch(friendDelete({list, index}))
+  }
+
+  function refuseHandler(){
+    dispatch(friendRefuse())
+  }
+  function acceptHandler(){
+    dispatch(friendAccept())
+  }
   return(
     <Row className='section__item-schedule'>
           <Col sm={3} className='text-center impo-margin-zero p-zero'>
@@ -18,11 +30,16 @@ function TabChildrenComponent(props){
             <Avatar className='' size={64} icon={<UserOutlined/>} />
           </Col>
           <Col sm={2} className='m-auto color-darkBlue p-zero'>
-          <p className='color-darkBlue text-left'>{user.userName}</p>
-        <p className='color-violet text-left'>{user.userEmail}</p></Col>
+            <p className='color-darkBlue text-left'>{user.userName}</p>
+            <p className='color-violet text-left'>{user.userEmail}</p></Col>
           <Col sm="auto">
-            <Button className='font-weight-800'>{i === 0 ? "살펴보기" : "친구수락"}</Button>
-            <Button className='margin-left bg-darkblue font-weight-800'>{i === 0 ? "친구삭제" : "거절하기"}</Button>
+            <Button className='font-weight-800'
+            onClick={i === 0 ? undefined : undefined}
+            >{i === 0 ? "살펴보기" : "친구수락"}</Button>
+            
+            <Button className='margin-left bg-darkblue font-weight-800'
+            onClick={() => i === 0 ? deleteHandler(list, index) : undefined}
+            >{i === 0 ? "친구삭제" : "거절하기"}</Button>
           </Col>
           <Col sm={3}>
           </Col>
@@ -31,8 +48,8 @@ function TabChildrenComponent(props){
 }
 
 function FriendBoard() {
-    const state = useSelector((state)=>state)
-
+    const state = useSelector((state)=>state.friendList)
+    const dispatch = useDispatch()
     return (
       <>
       <header>
@@ -55,23 +72,22 @@ function FriendBoard() {
                         <Col sm={3}></Col>
                         <Col className='mb-3 m-auto' sm={3}>{/** input칸 */}
                         <Form.Control
-                                type="eamil" 
-                                className='form-Control'
-                                placeholder='friend@gmail.com'
-                                onChange={(e)=>{
-                                }}
-                            />
+                          type="eamil" 
+                          className='form-Control'
+                          placeholder='Friend@email.com'
+                          onChange={(e)=>{console.log(e.target.value)}
+                        }/>
                         </Col>
                         <Col sm="auto">
-                          <Button>친구요청</Button>
+                          <Button onClick={(e)=>{}}>친구요청</Button>
                         </Col>
                         <Col sm={3}></Col>
                 </Form.Group>
               : ""}
               {
-                  state.friendList[i === 0 ? "userList" : "userRequest"].map((user, index) => {
+                  state[i === 0 ? "userList" : "userRequest"].map((user, index) => {
                     return (
-                      <TabChildrenComponent key={index} i={i} index={index} user={user}/>
+                      <TabChildrenComponent key={index} i={i} index={index} user={user} list={i === 0 ? "userList" : "userRequest"}/>
                     );
                   })
               }
