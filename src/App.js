78f1a528c7c8efe30481f505dfd0e6a4228fbed3
young24n <won8560@gmail.com>
2024-04-15 18:09:44 +0900

    route-TEST: 어떤 페이지든 렌더링 시 최초 1회 토큰 검사 및, 펫 유무 검사 없을 시 알맞은 페이지로 이동

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Signup from './pages/SignUp';
 import SignUpPet from './pages/SignUpPet';
 import Login from './pages/Login'
 import CalendarMain from './pages/CalendarMain';
 import FriendBoard from './pages/FriendsBoard'
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import serverUrl from './serverConfig';
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(()=>{//어느 페이지로 이동하든 
+      axios.get(`${serverUrl}/api/user/has-pet`
+      ,{withCredentials: true})
+      .then((response)=>{
+          if(response.data.messageDetail === "has"){
+              navigate('/calendar')//로그인 상태 + 펫있음
+          }else if(response.data.messageDetail === "nothing"){
+              navigate('/signup-pet')//로그인 상태 + 펫
+          }
+      })
+      .catch((error) => {
+          if(error.response.status === 401){//토큰 만료 리다이렉트
+              console.log("?" + error.status)
+              alert("로그인을 다시해주세요!")
+              navigate('/')
+          }
+      })
+  },[])//렌더링 최초 1회 
+
   return (
     <>
       <Routes>
