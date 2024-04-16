559fad2d568d1394e205676dc10bd3140be2134e
young24n <won8560@gmail.com>
2024-04-16 15:52:09 +0900

    petdex: Navbar add

--- src/pages/Petdex.js
+++ src/pages/Petdex.js
@@ -1,10 +1,14 @@
+import NavbarComponent from "../components/NavbarComponent";
+
 
 function Petdex(){
     return(
         <>
             <header>
+                <NavbarComponent/>
             </header>
             <body>
+                
             </body>
             <footer>
             </footer>
