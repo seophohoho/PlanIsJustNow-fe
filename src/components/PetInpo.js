250b66212ae675f3d2b670338c8e2a8530aea6be
young24n <won8560@gmail.com>
2024-04-19 18:19:14 +0900

    petdex: petinfo props 할당

--- src/components/PetInpo.js
+++ src/components/PetInpo.js
@@ -1,30 +1,17 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Form, Col, Button, Image, Stack } from 'react-bootstrap';
-import { selectPetName } from "../store/store"//수정할 함수 import 해야함
+import InputFieldComponent from "./InputFieldComponent";
 
 function PetInfo(props) {
-    const { petName, petInpo, onClick } = props;
+    const { cilckHandler, children, btnMessage } = props;
     const dispatch = useDispatch();
     return (
         <Col md="5">
             <Stack className='center margin-bottom-10'>
-                <Image src="/700x460.png" fluid />
-                <Stack direction='horizontal' gap={2} className='center margin-bottom-10'>
-                    <Form.Label column sm="4" className='color-darkBlue'>
-                        펫 이름
-                    </Form.Label>
-                    <Col sm="8">
-                        <Form.Control
-                            type="text"
-                            placeholder={petName}
-                            onChange={(e) => { dispatch(selectPetName(e.target.value)); }}
-                        />
-                    </Col>
-                </Stack>
-                <p className='color-lightPurple'>{petInpo}</p>
+                {children}
             </Stack>
-            <Button variant="primary" className='font-bold' onClick={onClick}>
-                이 펫으로 할래요!
+            <Button variant="primary" className='font-bold' onClick={cilckHandler}>
+                {btnMessage}
             </Button>
         </Col>
     );
