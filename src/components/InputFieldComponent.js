9143648fcd3a414074c8cd21bbbab3eacf8741df
young24n <won8560@gmail.com>
2024-04-19 20:04:25 +0900

    signUpPet: inputField value props 전달

--- src/components/InputFieldComponent.js
+++ src/components/InputFieldComponent.js
@@ -1,11 +1,13 @@
 import { Form } from 'react-bootstrap';
 
 function InputFieldComponent(props){
-    const { type, placeholder, onChangeHandler } = props
+    const { type, placeholder, onChangeHandler, value } = props
+
 
     return(
         <Form.Control
             type={type}
+            value={value}
             placeholder={placeholder}
             onChange={onChangeHandler} // 입력된 펫 이름 기억함수 필요
         />
