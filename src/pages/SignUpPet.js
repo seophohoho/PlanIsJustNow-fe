6e1a950c5ed63a2246b0fe0c1006663b55bdb12e
young24n <won8560@gmail.com>
2024-04-19 18:25:36 +0900

    signUpPet: petIndex 0으로 initialize, petInpo에 children을 전달하는 방식으로 변경, api path 변경

--- src/pages/SignUpPet.js
+++ src/pages/SignUpPet.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import serverUrl from "../serverConfig"
-import { Col, Row, Container, Navbar } from 'react-bootstrap';
+import { Col, Row, Container, Navbar, Image, Stack, Form } from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux"
 import PetInfo from '../components/PetInpo';
 import PetListMapComponent from '../components/PetListMapComponent.js';
 import chunkArray from '../function/chunkArray.js';
+import InputFieldComponent from '../components/InputFieldComponent.js';
+import { useEffect, useState } from 'react';
 
 //Todo 모든 post 버튼에 로딩 css 로직 추가
 function SignUpPet() {
-
-    const state = useSelector((state)=>{return state})//store에 있는 state 가져옴
+    const state = useSelector((state)=>{return state.petList})//store에 있는 state 가져옴
     const dispatch = useDispatch()//state변경 함수 사용할때 둘러야함
-    
+
+    const [selectedPetIndex, setSelectedPetIndex] = useState(0); // 선택된 펫 인덱스의 초기값 설정
+
     return (
         <div>
             <header>
@@ -31,16 +34,29 @@ function SignUpPet() {
                     <Container fluid>
                         <Row className='center'>
                             <Col md="7">
-                                <PetListMapComponent/>
-                            </Col>
-                            {chunkArray(state.petName, 12).map((petNamesChunk, chunkIndex) => (
-                                <PetInfo
-                                    key={chunkIndex}
-                                    petName={state.petName[chunkIndex]}
-                                    petInpo={state.petInpo[chunkIndex]}
-                                    onClick={() => { SelectBtnAct(state.petSelected.id, state.petSelected.name); }}
+                                <PetListMapComponent
+                                    petList={state}
+                                    selectedPetIndex={selectedPetIndex}
+                                    onSelectPet={setSelectedPetIndex}
                                 />
-                            ))}
+                            </Col>
+                                <PetInfo
+                                    btnMessage="이 펫으로 할래요!"
+                                    ClickHandler={() => { SelectBtnAct(); }}
+                                >
+                                    <Image src="/700x460.png" fluid />//경로필요
+                                    <Stack direction='horizontal' gap={2} className='center margin-bottom-10'>
+                                        <Form.Label column sm="4" className='color-darkBlue'>펫 이름</Form.Label>   
+                                        <Col sm="8">
+                                            <InputFieldComponent
+                                                type="text"
+                                                placeholder={state.data[selectedPetIndex].species}
+                                                onChangeHandler={""}
+                                            />
+                                        </Col>
+                                    </Stack>
+                                    <p className='color-lightPurple'>{state.data.info}</p>
+                                </PetInfo>
                         </Row>
                     </Container>
                 </div>
@@ -52,12 +68,14 @@ function SignUpPet() {
     );
 }
 
+//내부로 이동
 function SelectBtnAct(pet_id, pet_name){
-    axios.post(`${serverUrl}/api/user/choice-pet`,{
-        withCredentials: true,
-        "species" : pet_id,
-        "nickname" : pet_name
-    }).then(Response=>{
+    axios.post(`${serverUrl}/api/user/pet-signup`,
+    {withCredentials: true},
+    {
+
+    }
+    ).then(Response=>{
         console.log("yes")
     }).catch(error=>{
         console.log(error)
