49be9e91ace61e2ad7ae0e8ae5bc0768f827f643
young24n <won8560@gmail.com>
2024-04-19 18:21:33 +0900

    function: chunkArray 주석추가

--- src/function/chunkArray.js
+++ src/function/chunkArray.js
@@ -3,7 +3,7 @@ function chunkArray(arr, size) {
     const result = [];
     for (let i = 0; i < arr.length; i += size) {
         result.push(arr.slice(i, i + size));
-    }
+    }// size 만큼 배열을 나눠 줌
     return result;
 }
 
