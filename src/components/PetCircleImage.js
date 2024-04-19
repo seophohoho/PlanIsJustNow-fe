838edcd81a53aef8719971d82de7a0374a3ec6e5
young24n <won8560@gmail.com>
2024-04-19 18:18:46 +0900

    petdex: petCircleImage props 할당

--- src/components/PetCircleImage.js
+++ src/components/PetCircleImage.js
@@ -1,20 +1,20 @@
 import { Image, Stack } from 'react-bootstrap';
 
 function PetCircleImage(props){
-    const { petName, petId, isSelected } = props;
+    const { petName, isSelected, imagePath, onClick } = props;
 
     // isSelected 상태에 따라 동적으로 스타일 적용
     const selectedStyle = isSelected
         ? "pet-image border-outline-select"
         : "pet-image border-outline";
-
+        
     return (
         <Stack gap={1}>
             <Image
-                src={'/thumbnail.png'} // 이미지 디자인 완성시 -> state.petImages[i]로 변경 chunkIndex props로 받아와서 i에 적용
+                onClick={onClick}
                 roundedCircle
+                src={imagePath} //'/thumbnail.png' 이미지 디자인 완성시 -> state.petImages[i]로 변경 chunkIndex props로 받아와서 i에 적용
                 className={selectedStyle}
-                onClick={props.onClick}
             />
             <p className='pet-image color-lightPurple'>{petName}</p>
         </Stack>
