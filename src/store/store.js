820fb1f389966e25f89fafd891f6717f89124f22
young24n <won8560@gmail.com>
2024-04-19 18:47:28 +0900

    signUpPet: test data update

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,13 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 json object 만들 때 처음부터 2중으로 설계하는 것도 좋아보임
 dateSchedule을 년도->월->일 순으로 접근하게 구조를 변경하려했다가
 고칠게 너무 많아서 포기하게 됨
+
+redux는 그냥 상태관리 도구다. 컴포넌트 재사용 목적으로 props를 안쓴다?
+가능은 하지만 너무 맹목적이다. props와 같이 유연하게 쓰는 것이 더 올바르다.
+redux의 본질을 잊지 말자 -> 너무 아토믹하게 하면 뭐가뭔지 모르게 된다. 조심
+
+3항 연산자는 컴퓨터도 ㅈㄴ 싫어한다.
+퍼포먼스를 위해 앵간하면 줄여보자
 */
 const userPetData = createSlice({
   name: "userPetData",
@@ -10,19 +17,13 @@ const userPetData = createSlice({
     "data": [
       {
         "idx": 1,
-        "userId": {
-            "email": "testman@gmail.com",
-            "password": "$2a$10$MReG7/DP36f7ZFPxMNKEYu6lwjsBA./rrNq3OExhIqfftsUNTnq3K",
-            "nickname": "seophohoho",
-            "todolistFailureCount": 0
-        },
         "petId": {
-            "petId": 0,
-            "species": "펫_1"
+          "petId": 0,
+          "species": "펫_1"
         },
         "natureId": {
-            "natureId": 0,
-            "name": "장난꾸러기"
+          "natureId": 0,
+          "name": "장난꾸러기"
         },
         "petName": "꼬부기",
         "maxFriendship": 22616,
@@ -31,26 +32,25 @@ const userPetData = createSlice({
       },
       {
         "idx": 2,
-        "userId": {
-            "email": "testman@gmail.com",
-            "password": "$2a$10$MReG7/DP36f7ZFPxMNKEYu6lwjsBA./rrNq3OExhIqfftsUNTnq3K",
-            "nickname": "seophohoho",
-            "todolistFailureCount": 0
-        },
         "petId": {
-            "petId": 0,
-            "species": "펫_1"
+          "petId": 0,
+          "species": "펫_1"
         },
         "natureId": {
-            "natureId": 1,
-            "name": "활발한"
+          "natureId": 1,
+          "name": "활발한"
         },
         "petName": "꼬부기",
         "maxFriendship": 15624,
         "currentFriendship": 0,
         "runWayCount": 0
       },
-    ]
+    ],
+    "userId": {
+      "email": "testman@gmail.com",
+      "nickname": "seophohoho",
+      "todolistFailureCount": 0
+    }
   },
   reducers:{
     petdexInit(state, action){
@@ -59,49 +59,46 @@ const userPetData = createSlice({
   }
 })
 
-/*아래 분리된 펫 정보 병합 필요!*/
-const petImages = createSlice({//펫 이미지경로
-    name : 'petImages', 
-    initialState : ['path',], //state 정보(핵심)
-    //state 수정, 추가할 때 함수를 생성해서 여기서 실행시킴 외부파일에서는 그냥 해당함수 실행을 요청만함
-    reducers : { 
-    }
-})
-
-const petId = createSlice({//펫 정보
-  name : 'petId',
-  initialState : [0,],
-  reducers : {
-  }
-}) 
-
-const petName = createSlice({//펫 이름
-  name : 'petName',
-  initialState : ['햄톨이', "","","","","","","","","","",""],
-  reducers:{
-
-  }
-})
-
-const petInpo = createSlice({//펫 설명
-  name : 'petName',
-  initialState : [
-    '조그만 덩치에 걸맞지 않게 씩씩하고 시끄러운 햄스터!',
-  ],
-  reducers:{
-
-  }
-})
-
-const petSelected = createSlice({
-  name : "petSelected",
-  initialState : { id : -1 ,name : "petname"},
-  reducers:{
-    selectPetId(state, action){// 파라미터 설명 : state는 현재 본인이 담고있는 state를 가리킴, action은 요청시 외부에서 전달받은 데이터
-      console.log(state.id = action.payload) // action.payload : 전달받은 데이터를 이용할 때 payload를 작성하여 사용
-    },
-    selectPetName(state, action){
-      console.log(state.name = action.payload)
+const petList = createSlice({
+  name : 'petImages',
+  initialState : {
+    "data": [
+      {
+          "idx": 0,
+          "info": "커여운 커비좀 보세요.",
+          "path": "/thumbnail.png",
+          "species": "커비"
+      },
+      {
+          "idx": 1,
+          "info": "테스트2",
+          "path": "/thumbnail.png",
+          "species": "하하하하하"
+      }
+      ,
+      {
+          "idx": 2,
+          "info": "테스트3",
+          "path": "/thumbnail.png",
+          "species": "피카"
+      },
+      {
+          "idx": 3,
+          "info": "테스트4",
+          "path": "/thumbnail.png",
+          "species": "꼬부기"
+      },
+      {
+          "idx": 4,
+          "info": "테스트5",
+          "path": "/thumbnail.png",
+          "species": "????"
+      }
+    ]
+  },
+  reducer:{
+    petListInit(state, action){
+      return action.payload
     }
   }
 })
@@ -250,20 +247,16 @@ const friendList = createSlice({
 
 export const {addHandleClose, addHandleShow} = addShow.actions
 export const {scheduleInit, scheduleComplete, scheduleStateEdit, scheduleStateAdd, scheduleDelete} = dateSchedule.actions
-export const {selectPetId, selectPetName} = petSelected.actions
 export const {friendDelete, friendRefuse, friendAccept } = friendList.actions
 //함수또한 내보내야 요청가능
 
 export default configureStore({// 내보낼 state, 작성 문법은 아래와 같음
   reducer: { 
-    petImages : petImages.reducer,
-    petName : petName.reducer,
-    petId : petId.reducer,
-    petInpo : petInpo.reducer,
-    petSelected : petSelected.reducer,
     dateSchedule :dateSchedule.reducer,
     addShow : addShow.reducer,
     friendList : friendList.reducer,
+    petList : petList.reducer,
+    userPetData : userPetData.reducer,
     
   }
 }) 
