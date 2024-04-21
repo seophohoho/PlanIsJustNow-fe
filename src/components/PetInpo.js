a3cf20445ac8eb50004cd82ee3fe621819196c6b
young24n <won8560@gmail.com>
2024-04-21 11:10:44 +0900

    signUpPet: clickHandler 오타수정

--- src/components/PetInpo.js
+++ src/components/PetInpo.js
@@ -3,14 +3,14 @@ import { Form, Col, Button, Image, Stack } from 'react-bootstrap';
 import InputFieldComponent from "./InputFieldComponent";
 
 function PetInfo(props) {
-    const { cilckHandler, children, btnMessage } = props;
+    const { clickHandler, children, btnMessage } = props;
     const dispatch = useDispatch();
     return (
         <Col md="5">
             <Stack className='center margin-bottom-10'>
                 {children}
             </Stack>
-            <Button variant="primary" className='font-bold' onClick={cilckHandler}>
+            <Button variant="primary" className='font-bold' onClick={clickHandler}>
                 {btnMessage}
             </Button>
         </Col>
