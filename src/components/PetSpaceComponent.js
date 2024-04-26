858783e0cfe57f655586b36de9021940b610cd79
young24n <won8560@gmail.com>
2024-04-27 00:21:07 +0900

    줄바꿈 처리

--- src/components/PetSpaceComponent.js
+++ src/components/PetSpaceComponent.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Phaser from 'phaser';
 import {config} from './pet/constants/Game'
 
 function PetSpaceComponent() {
+
+
     useEffect(() => {
         console.log('????');
         const game = new Phaser.Game(config);
         return () => {
             game.destroy(true);
         };
-    }, []);
+    },[]);
 
     return (
         <div id='phaser-container'></div>  
