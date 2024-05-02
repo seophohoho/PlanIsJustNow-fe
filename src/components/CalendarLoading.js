1f0e4b58f46ca5a969e571021b3f93abf868197d
young24n <won8560@gmail.com>
2024-05-02 15:35:51 +0900

    route: suspense fallback 페이지 구현

---
+++ src/components/CalendarLoading.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
+import { Stack } from 'react-bootstrap';
+function CalendarLoading(){
+
+    return(
+    <div>
+        <header className='h-410'>
+        </header>
+        <body className='text-center'>
+            <Stack>
+            <Spin 
+            className='color-violet'
+            indicator={
+                <LoadingOutlined
+                    style={{fontSize: 72,}}
+                    spin/>}
+            />
+            <p className='color-darkBlue m-top-20'>Loading...</p>
+        </Stack>
+        </body>
+        <footer className='h-200'>
+
+        </footer>
+     </div>
+    )
+}
+
+export default CalendarLoading
