ce229ed738224b5d2f67166ac223d660c5a3b29b
young24n <won8560@gmail.com>
2024-05-06 22:01:51 +0900

    navbar: Link 방식에서 navigate로 변경

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Container, Navbar, Stack, Nav } from 'react-bootstrap';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const NavbarComponent = (props) => {
     const { userData } = props
+    const navigate = useNavigate()
     console.log(userData)
 
     return(
         <Navbar expand="md" className="bg-body-tertiary">{/**추후 Navbar도 컴포넌트화 해서 다른페이지에 적용시키기 */}
             <Container>
-                <Navbar.Brand href="/calendar">
+                <Navbar.Brand onClick={()=>{navigate("/calendar")}}>
                     <img src='/logo192.png'width={"50px"} className='m-auto'></img>
-                    <h1 style={{display: "inline"}} className=''>PETTODO</h1>
+                    <h1 style={{display: "inline", cursor: "Pointer"}} className='color-violet'>PETTODO</h1>
                 </Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">{/*추후 아이콘 추가*/}
-                    <Nav.Link href="/user-page">MyPage</Nav.Link>
-                    <Nav.Link href="/Friend-board">Friends</Nav.Link>
-                    <Nav.Link href="/petdex">Petdex</Nav.Link>
+                    <Nav.Link onClick={()=>{navigate("/user-page")}}>MyPage</Nav.Link>
+                    <Nav.Link onClick={()=>{navigate("/Friend-board")}}>Friends</Nav.Link>
+                    <Nav.Link onClick={()=>{navigate("/petdex")}}>Petdex</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
                 <Navbar.Text>
