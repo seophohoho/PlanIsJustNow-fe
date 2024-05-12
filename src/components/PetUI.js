c567f0412690401932fb0ee544c8c3c33d79b65c
young24n <won8560@gmail.com>
2024-05-12 22:24:25 +0900

    petui: 현재 호감도 stata 적용방식 변경

--- src/components/PetUI.js
+++ src/components/PetUI.js
@@ -4,8 +4,8 @@ import { HeartFilled } from "@ant-design/icons";
 import { IoHandLeftOutline } from "react-icons/io5";
 import { PiForkKnifeBold } from "react-icons/pi";
 function PetUI(props){
-    const {targetPet} = props
-    const percentSign = <span style={{ fontSize: "5px" }}>%</span>;
+    const {targetPet, currentFriendShip} = props
+    const percentSign = <span style={{ fontSize: "10px" }}>%</span>;
 
     function changeToPercent(current, max){
         return current / max * 100
@@ -16,18 +16,18 @@ function PetUI(props){
             <Row>
                 <Col sm={9}>
                     <Stack>
-                        <p className="color-darkBlue margin-left">이름 : {"name 바인딩"}</p>
+                        <p className="color-darkBlue margin-left">이름 : {targetPet[0].nickname}</p>
                     </Stack>
                     <Stack direction="horizontal" gap={2}>
                         <HeartFilled className="color-redfull font-size-20 margin-left"/>
                         <Stack>
-                            <p className="font-size-sm color-darkBlue font-weight-800">{targetPet[0].currentFriendShip} / {targetPet[0].maxFriendShip}</p>
+                            <p className="font-size-sm color-darkBlue font-weight-800">{currentFriendShip} / {targetPet[0].maxFriendShip}</p>
                             <Progress 
                             className="max-w-250 line-hight-1"
                             format={(percent) => <>{percent}{percentSign}</>}
                             percent={
                                 changeToPercent(
-                                    targetPet[0].currentFriendShip,
+                                    currentFriendShip,
                                     targetPet[0].maxFriendShip
                                 ).toFixed(1)
                             }
