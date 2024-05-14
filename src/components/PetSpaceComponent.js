e6b8fc2de8d30a375621c9b894fab76cdb7eda17
seophohoho <seop0937@gmail.com>
2024-05-14 20:43:36 +0900

    [feat:pet-runaway] Add pet run a way

--- src/components/PetSpaceComponent.js
+++ src/components/PetSpaceComponent.js
@@ -5,35 +5,34 @@ import {InitScene} from "./pet/scene/InitScene";
 import { CANVAS_WIDTH,CANVAS_HEIGHT } from './pet/constants/Game';
 
 function PetSpaceComponent(props) {
-    const {targetPetData,evolLevel} = props;
+    const {targetPetData,evolLevel,isPositiveFriendShip,currentFriendShip} = props;
 
     useEffect(() => {
         if(targetPetData !== undefined){
-            console.log('PetSpaceComponent 진화!!!');
-            console.log(targetPetData);
-            const config = {
-                type: Phaser.AUTO,
-                width: CANVAS_WIDTH,
-                height: CANVAS_HEIGHT,
-                render: {
-                    antialias: true, // 안티앨리어싱 활성화
-                    pixelArt: true // 픽셀 아트 게임이 아니라면 false로 설정
-                },
-                fps:{
-                    target: 60,
-                    forceSetTimeOut: true
-                },
-                backgroundColor: '#486870',
-                parent: 'phaser-container', // 게임을 렌더링할 요소의 ID를 지정합니다.
-                scene: [InitScene,PetSpaceScene],
-            };
-            InitScene.initData = targetPetData[0];
-            const game = new Phaser.Game(config);
-            return () => {
-                game.destroy(true);
-            };
-        }
-    },[evolLevel]);
+                const config = {
+                    type: Phaser.AUTO,
+                    width: CANVAS_WIDTH,
+                    height: CANVAS_HEIGHT,
+                    render: {
+                        antialias: true, // 안티앨리어싱 활성화
+                        pixelArt: true // 픽셀 아트 게임이 아니라면 false로 설정
+                    },
+                    fps:{
+                        target: 60,
+                        forceSetTimeOut: true
+                    },
+                    backgroundColor: '#486870',
+                    parent: 'phaser-container', // 게임을 렌더링할 요소의 ID를 지정합니다.
+                    scene: [InitScene,PetSpaceScene],
+                };
+                targetPetData[0].currentFriendShip = currentFriendShip;
+                InitScene.initData = targetPetData[0];
+                const game = new Phaser.Game(config);
+                return () => {
+                    game.destroy(true);
+                };
+            }
+    },[evolLevel,isPositiveFriendShip]);
 
     return (
         <div id='phaser-container'></div>  
