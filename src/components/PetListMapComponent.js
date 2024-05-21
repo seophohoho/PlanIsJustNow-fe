b85f944a1d25c6642087b9c47a467bbe08320086
young24n <won8560@gmail.com>
2024-05-21 19:44:54 +0900

    friend-board: commit 483ea98 cancel

--- src/components/PetListMapComponent.js
+++ src/components/PetListMapComponent.js
@@ -4,18 +4,17 @@ import PetCircleImage from "./PetCircleImage.js";
 
 function PetListMapComponent(props) {
     const { petList, selectedPetIndex, eventHandler } = props;
-
     return (
         <Stack direction="vertical" gap={1} className="margin-bottom-20">
             {chunkArray(petList, 4).map((petDataChunk, chunkIndex) => (
                 <Stack direction="horizontal" gap={1} className="margin-bottom-20" key={chunkIndex + 1}>
                     {petDataChunk.map((petData, index) => (
-                        petData.petId && (
+                        petData && (
                             <PetCircleImage
                                 key={index}
-                                petName={petData.petId.species ? petData.petId.species : petData.petId.nickname}
+                                petName={petData.species ? petData.species : petData.nickname}
                                 isSelected={(chunkIndex * 4 + index) === selectedPetIndex}
-                                imagePath={petData.petId.path ? petData.petId.path : '/thumbnail.png'}
+                                imagePath={petData.path ? petData.path : '/thumbnail.png'}
                                 onClick={() => eventHandler(chunkIndex, index)}
                             />
                         )
