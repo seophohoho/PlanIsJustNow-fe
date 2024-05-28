5d27a367533b1bcfe0d1e03059775695e9bb6dc5
young24n <won8560@gmail.com>
2024-05-28 16:08:05 +0900

    schedule: delete handler 코드 분할, axios 요청 동기처리, try-catch 구조 개선

--- src/components/ScheduleComponent.js
+++ src/components/ScheduleComponent.js
@@ -46,36 +46,14 @@ function Schedule(props){
     }, [ScheduleState.title]);
 
 
-    function scheduleDeleteHandler(){
-        /* confirm 추가 */
-        axios.put(`${serverUrl}/api/todolist/delete`, {
-            "idx" : state.dateSchedule[clickedDate][i].idx
-        },{withCredentials: true})
-        .then((response)=>{
-            console.log(response)
+    const scheduleDeleteHandler = async () => {
+        try{
+            const response = await axios.put(`${serverUrl}/api/todolist/delete`, { "idx" : state.dateSchedule[clickedDate][i].idx},{withCredentials: true})
             dispatch(scheduleDelete({index : i, clickedDate : clickedDate}))
-        })
-        .catch((error) => {
-            if(error.response){ // 런타임 에러방지 error.response가 있는지 먼저 확인함
-                if(error.response.status === 401) { // 토큰 만료 리다이렉트
-                    console.log("Error status: " + error.response.status);
-                    alert("로그인을 다시해주세요!");
-                    navigate('/');
-                }
-                else{
-                  alert("서버와 연결에 실패했습니다.");
-                }
-            }
-            else{
-                console.error("Error: ", error);
-                if(error.message) {
-                  alert("에러: " + error.message);
-                }
-                else{
-                  alert("알 수 없는 에러가 발생했습니다.");
-                }
-            }
-        });
+        }
+        catch (error){
+            handleError(error, navigate)
+        }
     }
 
     const confirmEvent = async () =>{
