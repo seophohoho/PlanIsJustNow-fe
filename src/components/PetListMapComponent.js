743c2d96f1848186bf7685d8754cdae040dd6674
young24n <won8560@gmail.com>
2024-05-28 16:01:33 +0900

    복층구조 개선

--- src/components/PetListMapComponent.js
+++ src/components/PetListMapComponent.js
@@ -5,23 +5,22 @@ import PetCircleImage from "./PetCircleImage.js";
 function PetListMapComponent(props) {
     const { petList, selectedPetIndex, eventHandler } = props;
     return (
-        <Stack direction="vertical" gap={1} className="margin-bottom-20">
-            {chunkArray(petList, 4).map((petDataChunk, chunkIndex) => (
-                <Stack direction="horizontal" gap={1} className="margin-bottom-20" key={chunkIndex + 1}>
-                    {petDataChunk.map((petData, index) => (
-                        petData && (
-                            <PetCircleImage
-                                key={index}
-                                petName={petData.species ? petData.species : petData.nickname}
-                                isSelected={(chunkIndex * 4 + index) === selectedPetIndex}
-                                imagePath={petData.path ? petData.path : '/thumbnail.png'}
-                                onClick={() => eventHandler(chunkIndex, index)}
-                            />
-                        )
-                    ))}
-                </Stack>
+    <Stack direction="vertical" gap={1} className="margin-bottom-20">
+        {chunkArray(petList, 4).map((petDataChunk, chunkIndex) => (
+            <Stack direction="horizontal" gap={1} className="margin-bottom-20" key={chunkIndex + 1}>
+            {petDataChunk.map((petData, index) => (
+                petData && (
+                    <PetCircleImage
+                    key={index}
+                    petName={petData.species ? petData.species : petData.nickname}
+                    isSelected={(chunkIndex * 4 + index) === selectedPetIndex}
+                    imagePath={petData.path ? petData.path : '/thumbnail.png'}
+                    onClick={() => eventHandler(chunkIndex, index)}
+                />)
             ))}
-        </Stack>
+            </Stack>
+        ))}
+    </Stack>
     );
 }
 
