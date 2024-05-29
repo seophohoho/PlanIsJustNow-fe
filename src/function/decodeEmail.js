4b4c77801ee7b98493d0fbb5046240b64a6fee0f
young24n <won8560@gmail.com>
2024-05-29 19:36:28 +0900

    주석추가

--- src/function/decodeEmail.js
+++ src/function/decodeEmail.js
@@ -1,13 +1,20 @@
 // base64, JS 기본 난독화 모듈 사용
 const decodeEmail = (obfuscatedEmail) => {
     try{
-        //해독한게 이메일 rex와 일치하는지 확인
-        //맞으면 -> 보내고 아니면 오류페이지로
-        return atob(obfuscatedEmail);
+        const regex =/^[a-zA-Z가-힣0-9]{2,13}$/;
+
+        const Email = atob(obfuscatedEmail)
+        
+        if(regex.test(Email)){
+            return Email
+        }
+        else{
+            //navigate error page
+        }
     }
     catch (error){
+        // status by navigate error page
 
-        //에러 발생시 유요하지 않는 접근 에러 페이지로
     }
 };
 
