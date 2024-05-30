739dce19aff0c173bc7fb2364d1c1951d7373952
young24n <won8560@gmail.com>
2024-05-30 16:26:38 +0900

    friend-detail: rex 조건 제거

--- src/function/decodeEmail.js
+++ src/function/decodeEmail.js
@@ -1,16 +1,8 @@
 // base64, JS 기본 난독화 모듈 사용
 const decodeEmail = (obfuscatedEmail) => {
     try{
-        const regex =/^[a-zA-Z가-힣0-9]{2,13}$/;
-
         const Email = atob(obfuscatedEmail)
-        
-        if(regex.test(Email)){
-            return Email
-        }
-        else{
-            //navigate error page
-        }
+        return Email
     }
     catch (error){
         // status by navigate error page
