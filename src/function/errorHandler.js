f6163e5e475d815c333e9215843bf7d1accb9b9d
young24n <won8560@gmail.com>
2024-05-31 17:51:50 +0900

    route: error page 구현, status, network 상태에 따라 적용되도록 구현

--- src/function/errorHandler.js
+++ src/function/errorHandler.js
@@ -1,20 +1,34 @@
-/* axios error handler */
 const handleError = (error, navigate) => {
-    if (error.response) { // error.response가 있는지 먼저 확인함
-      if (error.response.status === 401) { // 토큰 만료 리다이렉트
-        console.log("Error status: " + error.response.status);
-        alert("로그인을 다시해주세요!");
-        navigate('/sign-in');
-      } else {
-        alert("서버와 연결에 실패했습니다.");
-      }
-    } else {
-      if (error.message) {
-        alert("에러: " + error.message);
-      } else {
-        alert("알 수 없는 에러가 발생했습니다.");
-      }
+  if (error.response) { // error.response가 있는지 먼저 확인함
+    if (error.response.status === 401) { // 토큰 만료 리다이렉트
+      console.log("Error status: " + error.response.status);
+      alert("로그인을 다시해주세요!");
+      navigate('/sign-in');
+    } 
+    else if (error.response.status === 403) {
+      navigate('/connect-status', { state: { status: 403, statusTitle: 403, subTitle: '권한이 없습니다.' } });
+    } 
+    else if (error.response.status === 404) {
+      navigate('/connect-status', { state: { status: 404, statusTitle: 404, subTitle: '찾을 수 없는 페이지입니다.' } });
+    } 
+    else if (error.response.status === 405) {
+      navigate('/connect-status', { state: { status: 405, statusTitle: 405, subTitle: '허용되지 않은 메서드입니다.' } });
+    } 
+    else if (error.response.status === 500) {
+      navigate('/connect-status', { state: { status: 500, statusTitle: 500, subTitle: '서버 에러가 발생했습니다.' } });
+    } 
+    else {
+      alert("서버와 연결에 실패했습니다.");
     }
-  };
+  } else if (error.request) { // Network Error
+    navigate('/connect-status', { state: { statusTitle: 'Network Error', subTitle: '네트워크 오류가 발생했습니다. 다시 시도해 주세요.' } });
+  } else {
+    if (error.message) {
+      alert("에러: " + error.message);
+    } else {
+      alert("알 수 없는 에러가 발생했습니다.");
+    }
+  }
+};
 
-  export default handleError
+export default handleError;
