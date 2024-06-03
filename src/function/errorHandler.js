ac85bcc0dc662ccea666bf69c1a4f9d8123e1411
young24n <won8560@gmail.com>
2024-06-03 11:31:21 +0900

    errorHandler: alert가 동시다발 적으로 요청시 한번만 띄우도록 변경

--- src/function/errorHandler.js
+++ src/function/errorHandler.js
@@ -1,7 +1,18 @@
+let lastAlertTime = 0;
+const alertThrottleDuration = 1000; // 1초
+
+const showAlert = (message) => {
+  const currentTime = new Date().getTime();
+  if (currentTime - lastAlertTime > alertThrottleDuration) {
+    alert(message);
+    lastAlertTime = currentTime;
+  }
+};
+
 const handleError = (error, navigate) => {
   if (error.response) { // error.response가 있는지 먼저 확인함
     if (error.response.status === 401) { // 토큰 만료 리다이렉트
-      alert("로그인을 다시해주세요!");
+      showAlert("로그인을 다시해주세요!");
       navigate('/sign-in');
     } 
     else if (error.response.status === 403) {
@@ -17,15 +28,15 @@ const handleError = (error, navigate) => {
       navigate('/connect-status', { state: { status: 500, statusTitle: 500, subTitle: '서버 에러가 발생했습니다.' } });
     } 
     else {
-      alert("서버와 연결에 실패했습니다.");
+      showAlert("서버와 연결에 실패했습니다.");
     }
   } else if (error.request) { // Network Error
     navigate('/connect-status', { state: { statusTitle: 'Network Error', subTitle: '네트워크 오류가 발생했습니다. 다시 시도해 주세요.' } });
   } else {
     if (error.message) {
-      alert("에러: " + error.message);
+      showAlert("에러: " + error.message);
     } else {
-      alert("알 수 없는 에러가 발생했습니다.");
+      showAlert("알 수 없는 에러가 발생했습니다.");
     }
   }
 };
